e9252010ab08f50f1da72366c0e050c780e58d67
AGNESH K <agnesh9025@gmail.com>
2025-06-22 18:54:07 +0530

    Design Pen Implemented

---
+++ src/Pen.java
@@ -0,0 +1,59 @@
+
+
+
+public abstract class Pen {
+
+    protected String color;
+    protected String brand;
+
+    public Pen(String color, String brand) {
+        this.color = color;
+        this.brand = brand;
+    }
+     public abstract void write();
+
+    public static void main(String[] args) {
+        Pen pen1 = new BallPen("Reynolds", "Blue");
+        Pen pen2 = new FoundainPen("Parker", "Black");
+        Pen pen3 = new GelPen("galand", "Blue");
+        pen1.write();
+        pen2.write();
+        pen3.write();
+    }
+
+}
+ class BallPen extends Pen {
+
+    public BallPen(String color, String brand) {
+        super(color, brand);
+    }
+
+    @Override
+     public void write() {
+        System.out.println("BallPen is Written : " + brand);
+    }
+
+}
+class FoundainPen extends Pen {
+    public FoundainPen(String color, String brand) {
+        super(color, brand);
+    }
+
+    @Override
+    public void write() {
+        System.out.println("FoundainPen is Written : " + brand);
+    }
+}
+class GelPen extends Pen {
+    public GelPen(String color, String brand) {
+        super(color, brand);
+    }
+
+    @Override
+    public void write() {
+        System.out.println("GelPen is Writting Smoothly :" + brand);
+    }
+}
+
+
+
